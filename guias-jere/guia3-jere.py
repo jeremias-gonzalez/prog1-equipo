226c8c033d159d6168da14757dfdb8f773f3d3aa
jeremias-gonzalez <rusitgonzalez482@gmail.com>
2025-03-28 12:59:35 -0300

    main

---
+++ guias-jere/guia3-jere.py
@@ -0,0 +1,16 @@
+#1)
+
+# numero = int(input('ingrese un numero'))
+
+# for numero in range (0,10):
+#     numero = numero + 1
+#     print(numero)
+
+#2)
+
+#3)
+
+for numerosN in range(0,-9,-1):
+    print(numerosN)
+
+#4)
