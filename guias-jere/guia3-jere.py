981ff673a510e118cf3f41030558f4982fe1e456
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2025-03-29 05:08:27 -0300

    guia3 hasta el ejercicio 7 jere

--- guias-jere/guia3-jere.py
+++ guias-jere/guia3-jere.py
@@ -12,7 +12,7 @@
 #     numeroIngresado= int(input("ingresa un numero:"))
 #     if numeroIngresado > 23:
 #        contador = contador +1
-        
+
 
 # print(f"los numeros mayores a 23 son {contador}")
 
@@ -22,3 +22,47 @@
 #     print(numerosN)
 
 #4)
+
+# seguirIngresando = "si"
+# total = 0
+# while seguirIngresando == "si":
+#     ingresaSueldos = int(input("ingresa un sueldo:"))
+#     print(f"sueldo ingresado{ingresaSueldos}")
+#     ingresaSueldos = ingresaSueldos
+#     seguirIngresando = input("Quieres seguir ingresando sueldos? si/no:")
+#     total = total + ingresaSueldos
+# print(f"el total de sueldos ingresados es de:{total}")
+
+#5)
+#VERSION CHAT GPT Y YO.
+# pregunta = "si"
+# totalEdades = 0
+# cantidadPersonas = 0
+
+# while pregunta == "si":
+#     personas = int(input("¿Cuántas personas se van a cargar?: "))
+#     print(f"Genial, entonces son {personas} personas")
+    
+  
+#     for i in range(personas):
+#         edad = int(input(f"Ingresa la edad de la persona {i+1}: "))
+#         totalEdades = totalEdades + edad
+#         cantidadPersonas = cantidadPersonas + 1
+    
+    
+#     pregunta = input("¿Vas a seguir cargando personas? (si/no): ")
+
+
+# if cantidadPersonas > 0:
+#     promedio = int( totalEdades / cantidadPersonas)
+#     print(f"La edad promedio es: {promedio}")
+# else:
+#     print("No se han ingresado edades.")
+
+#6)
+# i = 5
+# for i in range (1,65):
+#     if i % 5 == 0:
+#         print(f"{i} es multiplo de 5")
+
+#7)
