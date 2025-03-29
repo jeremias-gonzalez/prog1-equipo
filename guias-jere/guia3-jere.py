d1c550d057c88e75282dd1267e6e62027ac7dd15
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2025-03-29 02:20:26 -0300

    main

--- guias-jere/guia3-jere.py
+++ guias-jere/guia3-jere.py
@@ -7,15 +7,14 @@
 #     print(numero)
 
 #2)
-contador=0
-for numeroIngresado in range(1,6):
-    numeroIngresado= int(input("ingresa un numero:"))
-    if numeroIngresado > 23:
-        numeroIngresadon= contador+1
-        contador= numeroIngresado
+# contador=0
+# for numeroIngresado in range(1,6):
+#     numeroIngresado= int(input("ingresa un numero:"))
+#     if numeroIngresado > 23:
+#        contador = contador +1
         
 
-print(f"los numeros mayores a 23 son {contador}")
+# print(f"los numeros mayores a 23 son {contador}")
 
 #3)
 
