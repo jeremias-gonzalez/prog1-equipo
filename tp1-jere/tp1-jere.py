d7363ed569b35f02d929c122d3c2979cf83ebbdb
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2025-05-05 21:50:38 -0300

    main

--- tp1-jere/tp1-jere.py
+++ tp1-jere/tp1-jere.py
@@ -2,10 +2,10 @@
 nombreCompleto=["Torres, Ana","Hudson, Kate","Quesada, Benicio","Campoamores, Susana","Santamaria, Carlos","Skarsgard, Azul","Catalejos, Walter"]
 departamento=["Logistica","Desarrollo","Desarrollo","Logistica","Administracion", "Logistica","Desarrollo"]
 fechaDeNacimiento=["15/05/1943","07/04/1984","10/02/1971","21/12/1967","30/01/1982","30/08/1995","18/07/1959"]
-resultado=""
-resultado2=""
-for i in nombreCompleto:
-    resultado = i[8:8]
-    resultado2 = i[0:8]
 
-print(f"{resultado}. {resultado2}")
+for i in nombreCompleto:
+     partes = i.split(", ")
+     inicial = partes[1][0]
+     apellido= partes[0][:11]
+     print(f"{inicial}. {apellido}")
+
