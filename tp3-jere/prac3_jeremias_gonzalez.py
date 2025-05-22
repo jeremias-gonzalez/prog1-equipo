28b2be155d67e2a5e3d09da160cdcdc17150a9fa
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2025-05-22 11:43:03 -0300

    main

--- tp3-jere/prac3_jeremias_gonzalez.py
+++ tp3-jere/prac3_jeremias_gonzalez.py
@@ -4,9 +4,13 @@ data2018=["Liam,19837,M","Noah,18267,M","Michael,14516,M","James,13525,M",
 nacidos2008="Eva,17039,M,Daniel,19005,M,Emily,17434,M,Emma,18813,M,Ethan,20216,M,Julia,18616,F,Jacob,22594,M,Joshua,19205,M,Michael,20626,M,Olivia,17081,F"
 nacidos2008Lista=nacidos2008.split(",")
 listaData2008=[]
+contador=0
 
 for i in nacidos2008Lista:
    nacidos2008Lista = i.split(",")
    listaData2008.append(nacidos2008Lista)
-
-print(listaData2008)
+   contador+=1
+   if contador==3:
+      contador=0
+      continue
+print(listaData2008,contador)
