55cd70d4da299a1714d75fe888813e837749f5a7
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2025-08-19 20:03:29 -0300

    prac 5 jere

--- tps-jere/tp5-jere/prac5_gonzalez_jeremias.py
+++ tps-jere/tp5-jere/prac5_gonzalez_jeremias.py
@@ -1,25 +1,27 @@
 
-with open("/workspaces/prog1-equipo/tps-jere/tp5-jere/clientes.txt", encoding="utf-8") as f:
+with open("tps-jere/tp5-jere/clientes.txt") as f:
     # Guardar cada línea en una lista, quitando saltos de línea
      lineas = f.readlines()
 
+localidades=[]
+
 def cantidadPersonas ():
     contador = 0
-    ingresarLocalidad=input("ingresa una localidad para saber la cantidad de personas:")
+    #ingresarLocalidad=input("ingresa una localidad para saber la cantidad de personas: ")
     for linea in lineas:
         partes = linea.split("#")
-        nuevaLista = partes
-        localidades = nuevaLista[3]
-        # mensaje=print()
-        pass
-    if ingresarLocalidad in localidades:
-             for localidad in localidades:
-                 if ingresarLocalidad == localidad:
-                  contador += 1
+        localidades.append(partes[3])
+        localidades.remove("\n")
+    mensaje = print(localidades)
+    
+     
+    # if ingresarLocalidad in localidades:
+    #          for localidad in localidades:
+    #              if ingresarLocalidad == localidad:
+    #               contador += 1
 
-                 mensaje = print(f"La cantidad de clientes en {ingresarLocalidad} es {contador}")
-    else:
-        print("Opción inválida")
+    #          mensaje = print(f"La cantidad de clientes en {ingresarLocalidad} es {contador}")
+                
 
     return mensaje
 
