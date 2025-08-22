a94bd756cf971c6d31e8547b0b2d5dfea17e7e3a
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2025-08-22 04:48:27 -0300

    jereputo

--- tps-peluca/tp5-peluca/prac5_olguin_marcos.py
+++ tps-peluca/tp5-peluca/prac5_olguin_marcos.py
@@ -1,6 +1,18 @@
 personas = []
 with open ('tps-peluca/tp5-peluca/clientes.txt', 'r', encoding="utf-8") as archivo:
+    next(archivo)  # Con esto salto la primera linea de la lista ('DNI,nombre y apellido,monto de deuda en pesos,localidad')
     for linea in archivo:
-        personas.append(linea.strip())
+        dni, nombre, deuda, localidad = linea.strip().split('#')
+        personas.append((dni, nombre, deuda, localidad))
 
 print (personas)
+
+# 1) La cantidad de clientes de ... (localidad literal y variable)
+def cantidad_clientes(localidad):
+    contador = 0
+    for persona in personas:
+        if persona[3] == localidad:
+            contador += 1
+    return contador
+
+print (f"La cantidad de clientes en la localidad especificada es: {cantidad_clientes('Villa María')}")
