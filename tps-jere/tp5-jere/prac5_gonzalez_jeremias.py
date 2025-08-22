195fd5996757399c554e416b2a6527976817743d
Jeremias González <126967561+jeremias-gonzalez@users.noreply.github.com>
2025-08-22 07:15:26 +0000

    main

--- tps-jere/tp5-jere/prac5_gonzalez_jeremias.py
+++ tps-jere/tp5-jere/prac5_gonzalez_jeremias.py
@@ -10,6 +10,23 @@ for i in range(len(lineas)):
     lista.append(lineas[i].split("#"))
 # print(lista)
 
+def apellidos():
+      lista_apellidos=[]
+      lista_dni=[]
+     
+      lista_conjunta=[]
+      for i in lista:
+          lista_dni.append(i[0])
+          lista_conjunta.append((i[0],i[1].split(" ")[1]))
+          salida = lista_conjunta
+          if i[0] and i[1] in salida :
+               dni = int(i[0])
+               if dni > 40000000:
+                 lista_apellidos.append(i[1])
+
+      return lista_apellidos
+print(apellidos())
+
 #---------------1)--------------------        
 # for i in lineas:
 #     localidades.append(i.split("#")[3])
