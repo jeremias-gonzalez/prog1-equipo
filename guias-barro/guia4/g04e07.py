6c5acdb6a83216003b9d741f93f4c6d626706da5
Juanchoxx151 <barrosoj502@gmail.com>
2025-08-28 21:31:39 -0300

    novedad

--- guias-barro/guia4/g04e07.py
+++ guias-barro/guia4/g04e07.py
@@ -3,14 +3,12 @@
 # mostrar un cartel diciendo que no lo ha encontrado.
 
 lista_valoresIguales = ['A', 'A', 'o', 'o', 'p', 'r', 'w', 'z', 'u', 'u']
-
-while True:
-    contador = 0    
+contador = 0
+while True:    
     valor_a_eliminar = input('¿Qué valor de la lista que sea igual desea eliminar?: ')
-    break
-for i in range(len(lista_valoresIguales)):
+
     if valor_a_eliminar in lista_valoresIguales:
-        if lista_valoresIguales[i] > 1:
-            contador += 1
-        lista_valoresIguales.remove(contador)
-print(lista_valoresIguales)        
+        for i in lista_valoresIguales:
+            if i == valor_a_eliminar:
+                contador += i
+                print(contador)
